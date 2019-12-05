6e2fb497eaa2adaf62aa2aee6b2c40db5689f563
Josef <juarisco@outlook.com>
2019-12-05 15:43:02 -0500

    09.-Sistema-de-autenticación

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,10 +8,6 @@ class TestController extends Controller
 {
     public function welcome()
     {
-        $a=5;
-        $b=11;
-        $c=$a+$b;
-
-        return "El valor de la suma es $c";
+        return view('welcome');
     }    
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@
 
 Route::get('/', 'TestController@welcome');
 
-Route::get('/prueba',function (){
-    return 'Hola soy una ruta de prueba';
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
