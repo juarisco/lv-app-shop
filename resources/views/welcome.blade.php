4ac13bb3ac140d2374f533bcc804a1077ebdc1a4
Josef <juarisco@outlook.com>
2019-12-05 15:04:35 -0500

    08.-Qué-son-los-controladores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Online Shop
                 </div>
 
                 <div class="links">
