563c57ac09f216acfaef672a71174209b8e63ed4
Josef <juarisco@outlook.com>
2019-12-07 17:31:53 -0500

    028.-Model-factories-con-relaciones

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,8 +11,23 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Category::class,5)->create();
-        factory(\App\Product::class, 100)->create();
-        factory(\App\ProductImage::class,200)->create();
+//        factory(\App\Category::class,5)->create();
+//        factory(\App\Product::class, 100)->create();
+//        factory(\App\ProductImage::class,200)->create();
+
+        $categories = factory(\App\Category::class, 5)->create();
+
+        $categories->each(function ($category) {
+            $products = factory(\App\Product::class, 20)->make();
+            $category->products()->saveMany($products);
+
+            $products->each(function ($p) {
+                $images = factory(\App\ProductImage::class, 5)->make();
+                $p->images()->saveMany($images);
+            });
+
+        });
+
+
     }
 }
