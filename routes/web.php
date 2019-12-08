db39527b209e37c57ca1d45764f5b67c2f5c647f
Josef <juarisco@outlook.com>
2019-12-08 15:51:12 -0500

    031.-CRUD-de-Productos

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', 'TestController@welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/products','ProductController@index');
+Route::get('/admin/products/create','ProductController@create');
+Route::post('/admin/products','ProductController@store');
