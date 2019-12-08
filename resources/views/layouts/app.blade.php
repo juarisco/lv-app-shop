f166450497456031853d6c55e209b6aab7385179
Josef <juarisco@outlook.com>
2019-12-08 11:51:08 -0500

    029.-Listar-productos-y-sus-imágenes-destacadas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 
-    <title>Sign Up Page - Material Kit by Creative Tim</title>
+    <title>@yield('title', 'App Shop')</title>
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
 
@@ -56,7 +56,8 @@
                                     Desconectarse
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
                                     {{ csrf_field() }}
                                 </form>
                             </li>
