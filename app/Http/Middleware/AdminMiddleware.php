c8ef7617d5a36d7501fd28ec63e16d7a0a687fc0
Josef <juarisco@outlook.com>
2019-12-11 11:24:25 -0500

    042.-Vista-home-y-directivas-condicionales

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -9,15 +9,12 @@ class AdminMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-//        if (!auth()->check() Or !auth()->user()->admin)
-//            return redirect('/login');
-
         if (!auth()->user()->admin)
             return redirect('/');
 
