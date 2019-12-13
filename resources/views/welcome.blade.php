f185ddd7ebc13a983497d345ad20767360e846dc
Josef <juarisco@outlook.com>
2019-12-13 14:58:32 -0500

    051.-Paginar-productos-y-corregir-problema-con-las-columnas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,27 @@
 
 @section('body-class','landing-page')
 
+@section('styles')
+    <style>
+        .team .row .col-md-4 {
+            margin-bottom: 5rem;
+        }
+
+        .row {
+            display: -webkit-box;
+            display: -webkit-flex;
+            display: -ms-flexbox;
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        .row > [class*='col-'] {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
+@endsection
+
 @section('content')
 
     <div class="header header-filter"
@@ -28,7 +49,8 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <h2 class="title">¿Por qué App Shop?</h2>
-                        <h5 class="description">Puedes revisar nuestra relación completa de productos, comparar precios y realizar tus pedidos cuando estés seguro.</h5>
+                        <h5 class="description">Puedes revisar nuestra relación completa de productos, comparar precios
+                            y realizar tus pedidos cuando estés seguro.</h5>
                     </div>
                 </div>
 
@@ -40,7 +62,8 @@
                                     <i class="material-icons">chat</i>
                                 </div>
                                 <h4 class="info-title">Atendemos tus dudas</h4>
-                                <p>Atendemos rápidamente cualquier consulta que tengas vía chat. No estás solo, sino que siempre estamos atentos a tus inquietudes.</p>
+                                <p>Atendemos rápidamente cualquier consulta que tengas vía chat. No estás solo, sino que
+                                    siempre estamos atentos a tus inquietudes.</p>
                             </div>
                         </div>
                         <div class="col-md-4">
@@ -49,7 +72,8 @@
                                     <i class="material-icons">verified_user</i>
                                 </div>
                                 <h4 class="info-title">Pago seguro</h4>
-                                <p>Todo pedido que realices será confirmado a través de una llamada. Si no confías en los pagos en línea puedes pagar contra entrega el valor acordado.</p>
+                                <p>Todo pedido que realices será confirmado a través de una llamada. Si no confías en
+                                    los pagos en línea puedes pagar contra entrega el valor acordado.</p>
                             </div>
                         </div>
                         <div class="col-md-4">
@@ -58,7 +82,8 @@
                                     <i class="material-icons">fingerprint</i>
                                 </div>
                                 <h4 class="info-title">Información privada</h4>
-                                <p>Los pedidos que realices sólo los conocerás tú a través de tu panel de usuario. Nadie más tiene acceso a esta información.</p>
+                                <p>Los pedidos que realices sólo los conocerás tú a través de tu panel de usuario. Nadie
+                                    más tiene acceso a esta información.</p>
                             </div>
                         </div>
                     </div>
@@ -74,19 +99,24 @@
                             <div class="col-md-4">
                                 <div class="team-player">
                                     <img src="{{ $product->featured_image_url }}" alt="Thumbnail Image"
-                                         class="img-circle">
-                                    <h4 class="title">{{ $product->name }} <br/>
+                                         class="img-raised img-circle">
+                                    <h4 class="title">
+                                        <a href="{{ url("'products/{$product->id}'") }}">{{ $product->name }}</a>
+                                        <br/>
                                         <small class="text-muted">{{ $product->category->name }}</small>
                                     </h4>
                                     <p class="description">{{ $product->description }}</p>
-                                    <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-twitter"></i></a>
-                                    <a href="#pablo" class="btn btn-simple btn-just-icon"><i
-                                                class="fa fa-instagram"></i></a>
-                                    <a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i
-                                                class="fa fa-facebook-square"></i></a>
+                                    {{--                                    <a href="#pablo" class="btn btn-simple btn-just-icon"><i class="fa fa-twitter"></i></a>--}}
+                                    {{--                                    <a href="#pablo" class="btn btn-simple btn-just-icon"><i--}}
+                                    {{--                                                class="fa fa-instagram"></i></a>--}}
+                                    {{--                                    <a href="#pablo" class="btn btn-simple btn-just-icon btn-default"><i--}}
+                                    {{--                                                class="fa fa-facebook-square"></i></a>--}}
                                 </div>
                             </div>
                         @endforeach
+                    </div>
+                    <div class="text-center">
+                        {{ $products->links() }}
                     </div>
                 </div>
 
@@ -97,7 +127,9 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <h2 class="text-center title">¿Aún no te has registrado?</h2>
-                        <h4 class="text-center description">Regístrate ingresando tus datos básicos, y podrás realizar tus pedidos a través de nuestro carrito de compras, Si aún no te decides, de todas formas, con tu cuenta de usuario podrás hacer todas tus consultas sin compromiso.</h4>
+                        <h4 class="text-center description">Regístrate ingresando tus datos básicos, y podrás realizar
+                            tus pedidos a través de nuestro carrito de compras, Si aún no te decides, de todas formas,
+                            con tu cuenta de usuario podrás hacer todas tus consultas sin compromiso.</h4>
                         <form class="contact-form">
                             <div class="row">
                                 <div class="col-md-6">
