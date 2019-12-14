9035597b613914a1ef46cdb18b8ab9b46dd1c086
Josef <juarisco@outlook.com>
2019-12-14 16:02:36 -0500

    057.-Una-vulnerabilidad-y-su-corrección

--- app/Http/Controllers/CartDetailController.php
+++ app/Http/Controllers/CartDetailController.php
@@ -9,10 +9,10 @@ class CartDetailController extends Controller
 {
     public function store(Request $request)
     {
-        $cartDetail=new CartDetail();
-        $cartDetail->cart_id=auth()->user()->cart->id;
-        $cartDetail->product_id=$request->product_id;
-        $cartDetail->quantity=$request->quantity;
+        $cartDetail = new CartDetail();
+        $cartDetail->cart_id = auth()->user()->cart->id;
+        $cartDetail->product_id = $request->product_id;
+        $cartDetail->quantity = $request->quantity;
         $cartDetail->save();
 
         return back();
@@ -20,8 +20,10 @@ class CartDetailController extends Controller
 
     public function destroy(Request $request)
     {
-        $cartDetail=CartDetail::findOrFail($request->cart_detail_id);
-        $cartDetail->delete();
+        $cartDetail = CartDetail::findOrFail($request->cart_detail_id);
+
+        if ($cartDetail->cart_id == auth()->user()->cart->id)
+            $cartDetail->delete();
 
         return back();
     }
