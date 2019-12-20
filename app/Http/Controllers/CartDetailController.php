ae7c823aa79faab98672f3f933aa14e02cf30f42
Josef <juarisco@outlook.com>
2019-12-20 14:59:53 -0500

    075.-Limitaciones-del-middleware-auth-para-peticiones-POST

--- app/Http/Controllers/CartDetailController.php
+++ app/Http/Controllers/CartDetailController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class CartDetailController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(Request $request)
     {
         $cartDetail = new CartDetail();
